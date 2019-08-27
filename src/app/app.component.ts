84fa3e725ac37e9b2f41a927077cf8c4b2c8daae
Dimitar Dimitrov <d.dimitrov899@gmail.com>
2019-08-27 15:29:50 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'i-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'webpack-project-single-page';
+}
